ab9ce62594468fb31af39c51c8a0495d750ded66
Archie Agarwal <archieagarwal2001@gmail.com>
2023-09-25 13:10:25 +0530

    ManageSeller

--- ManageSeller.java
+++ ManageSeller.java
@@ -1,15 +1,40 @@
 import javax.swing.*;
 import javax.swing.table.JTableHeader;
-
+import java.sql.*;
 import java.awt.*;
 import java.sql.DriverManager.*;
+import java.util.Vector;
 public class ManageSeller extends JFrame{
     JTable jtb;
     JTableHeader jth;
+    Vector row, heading, col;
     ManageSeller(){
         setSize(800, 600);
         setUndecorated(true);
-        jth=new JTableHeader()
+        Connection conn=null;
+        Statement stmt=null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket", "root", "");
+            stmt=conn.createStatement();
+            ResultSet rs=stmt.executeQuery("Select * from signup_details");
+            while(rs.next()){
+               col=new Vector<>();
+               col.add(rs.getString("Seller ID"));
+               col.add(rs.getString("Name"));
+               col.add(rs.getString("EmailID"));
+               col.add(rs.getString("Password"));
+               col.add(rs.getString("Phone"));
+               row.add(col);
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        jtb=new JTable(row, heading);
+        jth=jtb.getTableHeader();
         setVisible(true);
     }
     public static void main(String[] args){
