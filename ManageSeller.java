095afc9887701482ee7cdeffaef50ee8daf88db9
Archie Agarwal <archieagarwal2001@gmail.com>
2023-10-25 00:48:08 +0530

    Manage_Seller

--- ManageSeller.java
+++ ManageSeller.java
@@ -2,40 +2,60 @@ import javax.swing.*;
 import javax.swing.table.JTableHeader;
 import java.sql.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.DriverManager.*;
 import java.util.Vector;
-public class ManageSeller extends JFrame{
+public class ManageSeller extends JFrame implements ActionListener{
+    JPanel pnl, pnlbtn;
     JTable jtb;
     JTableHeader jth;
     Vector row, heading, col;
-    ManageSeller(){
-        setSize(800, 600);
-        setUndecorated(true);
-        Connection conn=null;
-        Statement stmt=null;
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket", "root", "");
-            stmt=conn.createStatement();
-            ResultSet rs=stmt.executeQuery("Select * from signup_details");
-            while(rs.next()){
-               col=new Vector<>();
-               col.add(rs.getString("Seller ID"));
-               col.add(rs.getString("Name"));
-               col.add(rs.getString("EmailID"));
-               col.add(rs.getString("Password"));
-               col.add(rs.getString("Phone"));
-               row.add(col);
-            }
-            rs.close();
-            stmt.close();
-            conn.close();
-        }catch(Exception e){
-            System.out.println(e.getMessage());
+    JButton jbtn;
+    public void actionPerformed(ActionEvent e){
+       pnl=new JPanel();
+       pnlbtn=new JPanel();
+       jbtn=new JButton("DELETE");
+       pnlbtn.add(jbtn);
+       setSize(800, 600);
+       setUndecorated(true);
+       row=new Vector<>();
+       col=new Vector<>();
+       heading=new Vector<>();
+       heading.add("Seller_Id");
+       heading.add("Seller Name");
+       heading.add("User ID");
+       heading.add("Password");
+       heading.add("Phone No.");
+       heading.add("");
+       Connection conn=null;
+       Statement stmt=null;
+       ResultSet rs=null;
+       try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket", "root", ""); 
+        stmt=conn.createStatement();
+        rs=stmt.executeQuery("Select * from signup_details");
+        while(rs.next()){
+           col=new Vector<>();
+           col.add(rs.getString("Seller ID"));
+           col.add(rs.getString("Name"));
+           col.add(rs.getString("EmailID"));
+           col.add(rs.getString("Password"));
+           col.add(rs.getString("Phone"));    
+           row.add(col);
         }
+        rs.close();
+        stmt.close();
+        conn.close();
         jtb=new JTable(row, heading);
         jth=jtb.getTableHeader();
-        setVisible(true);
+    }catch(Exception ee){
+        System.out.println(ee);
+    }
+       pnl.add(jtb);
+       add(pnl);
+       setVisible(true);
     }
     public static void main(String[] args){
         new ManageSeller();
